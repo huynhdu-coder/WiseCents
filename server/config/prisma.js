4ad54c0dd4429e4dba0a25ef7b0ec53beac0c445
Dai Huynh <hdai2003@gmail.com>
2026-03-01 22:46:19 -0500

    Upgrading Prisma to v6

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -2,8 +2,6 @@ import pkg from "@prisma/client";
 
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+const prisma = new PrismaClient();
 
 export default prisma;
